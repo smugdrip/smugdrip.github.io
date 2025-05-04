dabb56613b1c01c9fe36e58d810be7801bafcea2
smugdrip <jpmcb141@gmail.com>
2025-05-04 11:06:53 -0400

    changed to use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Ex from './pages/Ex.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Ex" element={<Ex />} />
       </Routes>
-    </BrowserRouter >
+    </HashRouter >
   </StrictMode>,
 )
